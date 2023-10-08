dbf59693d1eefaff071d783387ba111458e224cc
Cristian Villa <c.villavirgen@gmail.com>
2023-10-07 22:41:28 -0600

    Add optional parameters, parent permission, and service eligibility methods to Person class

--- person.rb
+++ person.rb
@@ -1,10 +1,12 @@
 class Person
-    def initialize(id, name, age)
-      @id = id
+    def initialize(name = "Unknown", age, parent_permission = true)
+      @id = Random.rand(1..1000)
       @name = name
       @age = age
+      @parent_permission = parent_permission
     end
-    
+  
+    # Getter methods
     def id
       @id
     end
@@ -16,13 +18,26 @@ class Person
     def age
       @age
     end
-
+  
+    # Setter methods
     def name=(new_name)
-        @name = new_name
-      end
-      
-      def age=(new_age)
-        @age = new_age
-      end
+      @name = new_name
+    end
+    
+    def age=(new_age)
+      @age = new_age
+    end
+  
+    # Public method to check if the person can use services
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    private
+  
+    # Private method to check if the person is of age
+    def of_age?
+      @age >= 18
+    end
   end
   
