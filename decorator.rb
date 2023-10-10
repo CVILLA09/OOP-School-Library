181ddb8f9f1fb824868304dd82443b7a750ae9de
Cristian Villa <c.villavirgen@gmail.com>
2023-10-09 21:08:11 -0600

    Fix linters

--- decorator.rb
+++ decorator.rb
@@ -5,6 +5,7 @@ require_relative 'nameable'
 # This class serves as the base decorator for other specific decorators.
 class BaseDecorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
   end
 
