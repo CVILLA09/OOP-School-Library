8753d9f4609ca8b5f5ea4e7d2d3e5b498d10d469
Cristian Villa <c.villavirgen@gmail.com>
2023-10-11 20:27:26 -0600

    Add method to list all people in App class

--- app.rb
+++ app.rb
@@ -2,4 +2,8 @@ class App
     def list_all_books
       puts "List of all books will be displayed here."
     end
+  
+    def list_all_people
+      puts "List of all people will be displayed here."
+    end
   end
