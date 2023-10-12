a386626a2339e98319a604425855f9986aee86c7
Cristian Villa <c.villavirgen@gmail.com>
2023-10-11 21:19:05 -0600

    Update main.rb to match the School Library UI video example

--- main.rb
+++ main.rb
@@ -1,16 +1,19 @@
 require_relative 'app'
 
 def main
+  puts "Welcome to School Library App!"
+
   app = App.new
 
   loop do
-    puts "Please choose an option:"
-    puts "1. List all books"
-    puts "2. List all people"
-    puts "3. Create a person"
-    puts "4. Create a book"
-    puts "5. Create a rental"
-    puts "6. Exit"
+    puts "\nPlease choose an option by entering a number:"
+    puts "1 - List all books"
+    puts "2 - List all people"
+    puts "3 - Create a person"
+    puts "4 - Create a book"
+    puts "5 - Create a rental"
+    puts "6 - List all rentals for a given person id"
+    puts "7 - Exit"
 
     choice = gets.chomp
 
@@ -26,7 +29,9 @@ def main
     when '5'
       app.create_rental
     when '6'
-      puts "Exiting..."
+      app.list_all_rentals_for_person
+    when '7'
+      puts "Thank you for using this app!"
       break
     else
       puts "Invalid option. Please try again."
