402d5afa9863526675b0271204884ebc38114b8a
Cristian Villa <c.villavirgen@gmail.com>
2023-10-11 21:29:00 -0600

    Implement functionality for creating a person (Student or Teacher)

--- app.rb
+++ app.rb
@@ -28,7 +28,41 @@ class App
     end
   
     def create_person
-      puts "A new person will be created here."
+      puts "Do you want to create a student (1) or a teacher (2)? [Input the number]:"
+      choice = gets.chomp
+  
+      case choice
+      when '1'
+        create_student
+      when '2'
+        create_teacher
+      else
+        puts "Invalid option. No person was created."
+      end
+    end
+  
+    def create_student
+      print "Age: "
+      age = gets.chomp
+      print "Name: "
+      name = gets.chomp
+      print "Has parent permission? [Y/N]: "
+      parent_permission = gets.chomp.downcase == 'y'
+  
+      # Logic to create a student
+      puts "Student created successfully"
+    end
+  
+    def create_teacher
+      print "Age: "
+      age = gets.chomp
+      print "Name: "
+      name = gets.chomp
+      print "Specialization: "
+      specialization = gets.chomp
+  
+      # Logic to create a teacher
+      puts "Teacher created successfully"
     end
   
     def create_book
