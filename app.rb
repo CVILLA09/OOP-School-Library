ec5416a7101d7a5d4895fd8efe3a33a6bc3f9804
Feliperto <fperezferraro@gmail.com>
2023-10-17 20:49:15 -0300

    added the load and save methods

--- app.rb
+++ app.rb
@@ -5,16 +5,14 @@ require_relative 'teacher'
 require_relative 'rental_manager'
 require_relative 'book_manager'
 require_relative 'person_manager'
-
 # Class containing app functionality for a basic UI
 class App
   def initialize
     puts 'Welcome to School Library App!'
-    @book_manager = BookManager.new 
+    @book_manager = BookManager.new
     @person_manager = PersonManager.new
     @rental_manager = RentalManager.new(@book_manager, @person_manager)
   end
-
   def create_person_action
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     choice = gets.chomp
@@ -32,7 +30,6 @@ class App
                       end
     @person_manager.create_person(choice, age, name, additional_info)
   end
-
   def create_book_action
     print 'Title: '
     title = gets.chomp
@@ -40,22 +37,16 @@ class App
     author = gets.chomp
     @book_manager.create_book(title, author)
   end
-
   def create_rental_action
     return unless @rental_manager.valid_conditions_for_rental?
-
     book_index = @rental_manager.select_book
     return if book_index.nil?
-
     person_index = @rental_manager.select_person
     return if person_index.nil?
-
     print "\nDate: "
     date = gets.chomp
-
     @rental_manager.create_new_rental(book_index, person_index, date)
   end
-
   def handle_user_choice(choice)
     actions = {
       '1' => -> { @book_manager.list_all_books },
@@ -65,13 +56,13 @@ class App
       '5' => -> { create_rental_action },
       '6' => -> { @rental_manager.list_all_rentals_for_person },
       '7' => lambda {
-        @person_manager.save_people_to_json 
+        @person_manager.save_people_to_json
         @book_manager.save_books_to_json
+        @rental_manager.save_rentals_to_json
                puts 'Thank you for using this app!'
                exit
              }
     }
-
     if actions.key?(choice)
       actions[choice].call
     else
