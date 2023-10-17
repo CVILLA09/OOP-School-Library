b2c5f53948e0c3c5c34c6e0b0ce2fa98ea3f2bdf
Feliperto <fperezferraro@gmail.com>
2023-10-17 20:35:38 -0300

    Update rental to preserve data

--- rental.rb
+++ rental.rb
@@ -1,12 +1,19 @@
-# Class containing data about book rentals
 class Rental
   attr_accessor :date, :book, :person
-
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
     book.add_rental(self)
     person.add_rental(self)
+  end
+  def to_json(*_args)
+    {
+      'JSON_CLASS' => self.class.name,
+      'date' => @date,
+      'book_title' => @book.title,
+      'book_author' => @book.author,
+      'person_id' => @person.id
+    }.to_json
   end
 end
